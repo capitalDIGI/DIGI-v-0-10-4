03fe26896e5a90ddfef2620d57ce079d012d2946
Damian Obernikowicz <damian.obernikowicz@gmail.com>
2016-03-21 15:35:40 +0100

    DIGI

--- src/clientversion.h
+++ src/clientversion.h
@@ -14,7 +14,7 @@
  */
 
 //! These need to be macros, as clientversion.cpp's and bitcoin*-res.rc's voodoo requires it
-#define CLIENT_VERSION_MAJOR 1
+#define CLIENT_VERSION_MAJOR 0
 #define CLIENT_VERSION_MINOR 10
 #define CLIENT_VERSION_REVISION 4
 #define CLIENT_VERSION_BUILD 0
